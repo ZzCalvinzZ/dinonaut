4843429916200cca023eba7c174c58afd13d818d
Calvin Collins <calvinkcollins@gmail.com>
2017-04-21 10:27:48 -0400

    initial commit

---
+++ assets/js/main/index.js
@@ -0,0 +1,10 @@
+var renderer = PIXI.autoDetectRenderer(256, 256);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view);
+
+//Create a container object called the `stage`
+var stage = new PIXI.Container();
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage);
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var path = require("path");
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = modulePaths => ({
+    context: __dirname,
+
+    devtool: 'source-map',
+
+    entry: {
+        vendor: [
+            "expose-loader?PIXI!./assets/js/vendor/pixi.min.js",
+            'webpack-dev-server/client?http://localhost:20069',
+            'babel-polyfill',
+            './assets/js/vendor/index',
+        ],
+        main: [
+            './assets/js/main/index'
+        ]
+    },
+
+    output: {
+        publicPath: 'http://localhost:20069/assets/bundles/',
+        path: path.resolve('./assets/bundles/'),
+        filename: "[name].js",
+    },
+
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new ExtractTextPlugin('[name].css'),
+    ],
+
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'stage-2']
+            }
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true,
+                    }
+                } ,{
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true,
+                        includePaths: modulePaths
+                    }
+                }]
+            }),
+        },{
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('css-loader')
+        }],
+    },
+
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'assets/js/'),
+            path.resolve(__dirname, 'assets/style/'),
+        ],
+        extensions: ['*', '.js', '.scss', 'css'],
+    },
+})
