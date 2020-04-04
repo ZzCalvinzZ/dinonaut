dab79278f61c9a6e3e419ef3c19162e43f302307
Calvin Collins <calvinc@surveymonkey.com>
2020-04-04 11:55:10 -0400

    Updates

---
+++ precache-manifest.91324d9ab5b7fb41a806b4b991f4e6a3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe724f0df264ae6d3fe1ba0ea8176776",
+    "url": "/dinonaut/background/background.json"
+  },
+  {
+    "revision": "5f5cb8ab36f11121f17be3323a758b2d",
+    "url": "/dinonaut/background/background.png"
+  },
+  {
+    "revision": "f5281771b9b4385e0dca8e1ceb3c5cf4",
+    "url": "/dinonaut/dinonaut.png"
+  },
+  {
+    "revision": "c76f3a8cb1e0af62e0778f4feb3d1cd9",
+    "url": "/dinonaut/dinonautshield/dinonautshield.json"
+  },
+  {
+    "revision": "1a155cbe959a2796500764683c8308e5",
+    "url": "/dinonaut/dinonautshield/dinonautshield.png"
+  },
+  {
+    "revision": "05050c7e271215a4030714bce237649f",
+    "url": "/dinonaut/dinonauttheme.mp3"
+  },
+  {
+    "revision": "e824f127a76d8b18228093ef55ef69ba",
+    "url": "/dinonaut/dinonauttheme.ogg"
+  },
+  {
+    "revision": "af22ccb55efb6860f68c353383ac6fb1",
+    "url": "/dinonaut/dinonautwalking/dinonautwalking.json"
+  },
+  {
+    "revision": "245ee7e95a2831272b6300d662cbafdb",
+    "url": "/dinonaut/dinonautwalking/dinonautwalking.png"
+  },
+  {
+    "revision": "b9f926e4883cee5a1d065455c32385df",
+    "url": "/dinonaut/explosion1/explosion1.json"
+  },
+  {
+    "revision": "9e6f922c734861530cfb915021db8554",
+    "url": "/dinonaut/explosion1/explosion1.png"
+  },
+  {
+    "revision": "6932b011e319011cba31cc7df4f74ae4",
+    "url": "/dinonaut/explosionsound.mp3"
+  },
+  {
+    "revision": "7b7e42a3cf7ab62f569a58be85bfe418",
+    "url": "/dinonaut/index.html"
+  },
+  {
+    "revision": "68be1ef56bb837ef47b7ac3b9207db79",
+    "url": "/dinonaut/ludumdaretrack.mp3"
+  },
+  {
+    "revision": "95ef7bfe54433f5267130eb376eb4c2c",
+    "url": "/dinonaut/meteor.png"
+  },
+  {
+    "revision": "304e1089c73a86d852fc1c09395d70d1",
+    "url": "/dinonaut/meteor/meteor.json"
+  },
+  {
+    "revision": "7f73a361a7185f89aafe18fef06eff89",
+    "url": "/dinonaut/meteor/meteor.png"
+  },
+  {
+    "revision": "66e868910af98b301b253fe60c7a6d2a",
+    "url": "/dinonaut/planet.png"
+  },
+  {
+    "revision": "b006268b9b53a8ada84c1caf0cf79a42",
+    "url": "/dinonaut/point.mp3"
+  },
+  {
+    "revision": "833224f6d8e80ce3e67bd1dbb3b2d5e0",
+    "url": "/dinonaut/shield.mp3"
+  },
+  {
+    "revision": "ed4d90a746bff1609225d5b404b14981",
+    "url": "/dinonaut/speaker.png"
+  },
+  {
+    "revision": "2762dd6d601f033a1a0a1e66b85fec38",
+    "url": "/dinonaut/speaker_off.png"
+  },
+  {
+    "revision": "c541c96d6f71f8e9d505",
+    "url": "/dinonaut/static/css/main.79eb8662.chunk.css"
+  },
+  {
+    "revision": "528739e30452a4a2b666",
+    "url": "/dinonaut/static/js/2.7a5cde99.chunk.js"
+  },
+  {
+    "revision": "b0bd15d845192c39a3b30b8ab2d7221e",
+    "url": "/dinonaut/static/js/2.7a5cde99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c541c96d6f71f8e9d505",
+    "url": "/dinonaut/static/js/main.65f6d97b.chunk.js"
+  },
+  {
+    "revision": "e21389be851b5624e3e0",
+    "url": "/dinonaut/static/js/runtime-main.0439cc07.js"
+  }
+]);
